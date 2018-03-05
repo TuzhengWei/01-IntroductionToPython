7ef66ebb129a8fa83bdae63cf6ea45c4ae975309
weit <weit@rose-hulman.edu>
2018-03-05 09:01:08 -0500

    M2 HELLOW WORLD

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -1,4 +1,4 @@
-print('Hello, World')
+print(3607*34227 )
 
 ###############################################################################
 #
@@ -17,7 +17,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window and selecting
@@ -32,7 +32,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -40,7 +40,7 @@ print('Hello, World')
 #        by clicking on the blue bars.  ** Try that now. **
 #
 #     b. When you have completed a TO-DO, you should change the word
-#          _TODO_      (ignore the underscores on this line)
+#          _TODO_      (ignore the underscores/ on this line)
 #        to
 #          DONE
 #        Try it now on line 20 above, and note that its blue bar on
@@ -57,7 +57,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below the current Line 1 above.
 #   It should print any string that you want (but keep it G-rated!)
 #
@@ -70,7 +70,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
