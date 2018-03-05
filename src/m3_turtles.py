36cb44a03bb94427bd7dd9577c0b6c128f996af4
weit <weit@rose-hulman.edu>
2018-03-05 09:23:03 -0500

    M2 HELLOW WORLD

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Tuzheng Wei.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -49,7 +49,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving, *click anywhere in the window to close it*.
 #
@@ -105,7 +105,7 @@ natasha.forward(50)
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more line of your own code to make one of the
 #   existing SimpleTurtles move some more and/or have different
 #   characteristics.
@@ -114,7 +114,12 @@ natasha.forward(50)
 #      ** A SUBSEQUENT exercise will let you show your creativity. **
 #
 #   As always, test by running the module.
-#
+Henry = rg.SimpleTurtle('turtle')
+Henry.pen = rg.Pen('blue',40)
+Henry.speed = 20
+
+Henry.forward(90)
+Henry.left(90)
 ###############################################################################
 
 ###############################################################################
@@ -141,7 +146,14 @@ natasha.forward(50)
 #      ** A SUBSEQUENT exercise will let you show your creativity. **
 #
 #   As always, test by running the module.
-#
+henry = rg.SimpleTurtle('turtle')
+henry.pen = rg.Pen('blue',40)
+henry.speed = 20
+
+henry.forward(90)
+henry.left(90)
+henry.backward(90)
+henry.right(90)
 ###############################################################################
 
 ###############################################################################
